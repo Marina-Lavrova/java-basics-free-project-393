13863ca9b5a66bdd550470b2473138e2360ef473
Marina-Lavrova <68327666+Marina-Lavrova@users.noreply.github.com>
2026-08-03 12:01:56 +0000

    Generator ready

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(PasswordGenerator.generatePassword(5,true,false));
-        System.out.println(PasswordGenerator.generatePassword(20,true,true));
+        /*System.out.println(PasswordGenerator.generatePassword(35,true,true,true));
+        System.out.println(PasswordGenerator.generatePassword(20,true,true,true));*/
+        
+        System.out.println(PasswordGenerator.generatePassword(-10, 1L, true, true, false));
+        //System.out.println(PasswordGenerator.generatePassword(12, 123L, true, true, false));
+        //System.out.println(PasswordGenerator.generatePassword(12, 123L, true, true, true));
+        //System.out.println(PasswordGenerator.generatePassword(8, 1L, false, false, false));
     }
 }
--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -2,15 +2,42 @@ public class PasswordGenerator {
     private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
     private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "!@#$%^&*";
 
-    public static String generatePassword(int length, boolean useUppercase, boolean useDigits) {
+    private static long nextRandom(long number) {
+        return (16807L * number) % 2147483647L;
+    }
+
+    public static String generatePassword(int length, long seed, boolean useUppercase, boolean useDigits, boolean useSpecial) {
         StringBuilder result = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                int index = (int) (Math.random() * LOWERCASE.length());
-                char chRandomLow = (char) (Math.random() * LOWERCASE.length());
-                char chRandomUpper = (char) (Math.random() * UPPERCASE.length());
-                char chRandomDigit = (char) (Math.random() * DIGITS.length());
-                result.append(LOWERCASE.charAt(index));
+        
+        //if (length <=0){return "";}
+
+        String alphabet = LOWERCASE;
+    
+                if (useUppercase) {
+                    alphabet += UPPERCASE;
+                }
+                
+                if (useDigits) {
+                    alphabet += DIGITS;
+                }
+                
+                if (useSpecial) {
+                    alphabet += SPECIAL;
+                }
+
+        long current = seed;
+
+        for (int i = 0; i < length; i++) {
+                
+                //int index = (int) (Math.random() * alphabet.length());                
+
+
+                int index = (int)(current % alphabet.length());
+                current = nextRandom(current);
+
+                result.append(alphabet.charAt(index));
             }
             return result.toString();
     }
