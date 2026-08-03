a65b0d24e6cd0ae89d4a7c0fadd52f56b1d36eaa
Marina-Lavrova <68327666+Marina-Lavrova@users.noreply.github.com>
2026-08-03 12:49:18 +0000

    Generator passwords and validator

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println(PasswordGenerator.generatePassword(35,true,true,true));
-        System.out.println(PasswordGenerator.generatePassword(20,true,true,true));*/
-        
-        //System.out.println(PasswordGenerator.generatePassword(-10, 1L, true, true, false));
-        //System.out.println(PasswordGenerator.generatePassword(12, 123L, true, true, false));
-        //System.out.println(PasswordGenerator.generatePassword(12, 123L, true, true, true));
-        //System.out.println(PasswordGenerator.generatePassword(8, 1L, false, false, false));
+        System.out.println("== Генерация паролей ==");
+        System.out.println("буквы и цифры:    " + PasswordGenerator.generatePassword(12, 123L, true, true, false));
+        System.out.println("со спецсимволами: " + PasswordGenerator.generatePassword(16, 7L, true, true, true));
 
-        System.out.println(PasswordGenerator.checkPassword("abc"));
-        System.out.println(PasswordGenerator.checkPassword("abcdefgh"));
-        System.out.println(PasswordGenerator.checkPassword("abcdef1234"));
-        System.out.println(PasswordGenerator.checkPassword("Abcdef1234"));
-        System.out.println(PasswordGenerator.checkPassword("Abcdef123!"));
+        System.out.println();
+        System.out.println("== Проверка надёжности ==");
+        System.out.println("abc        -> " + PasswordGenerator.checkPassword("abc"));
+        System.out.println("abcdef1234 -> " + PasswordGenerator.checkPassword("abcdef1234"));
+        System.out.println("Abcdef123! -> " + PasswordGenerator.checkPassword("Abcdef123!"));
     }
 }
