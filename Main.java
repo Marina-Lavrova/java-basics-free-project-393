c0656ae30a2f0ee2318cd2bc6a315459ff30fe04
Marina-Lavrova <68327666+Marina-Lavrova@users.noreply.github.com>
2026-08-03 15:06:37 +0000

    checkstyle fix

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("== Генерация паролей ==");
-        System.out.println("буквы и цифры:    " + PasswordGenerator.generatePassword(12, 123L, true, true, false));
-        System.out.println("со спецсимволами: " + PasswordGenerator.generatePassword(16, 7L, true, true, true));
+        System.out.println(
+            "буквы и цифры: "
+            + PasswordGenerator.generatePassword(12, 123L, true, true, false));
+        System.out.println(
+            "со спецсимволами: "
+            + PasswordGenerator.generatePassword(16, 7L, true, true, true));
 
         System.out.println();
         System.out.println("== Проверка надёжности ==");
-        System.out.println("abc        -> " + PasswordGenerator.checkPassword("abc"));
-        System.out.println("abcdef1234 -> " + PasswordGenerator.checkPassword("abcdef1234"));
-        System.out.println("Abcdef123! -> " + PasswordGenerator.checkPassword("Abcdef123!"));
+        System.out.println("abc -> " + PasswordGenerator.checkPassword("abc"));
+        System.out.println(
+            "abcdef1234 -> " + PasswordGenerator.checkPassword("abcdef1234"));
+        System.out.println(
+            "Abcdef123! -> " + PasswordGenerator.checkPassword("Abcdef123!"));
     }
 }
--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -8,45 +8,51 @@ public class PasswordGenerator {
         return (16807L * number) % 2147483647L;
     }
 
-    public static String generatePassword(int length, long seed, boolean useUppercase, boolean useDigits, boolean useSpecial) {
+    public static String generatePassword(
+        int length,
+        long seed,
+        boolean useUppercase,
+        boolean useDigits,
+        boolean useSpecial
+    ) {
         StringBuilder result = new StringBuilder();
-        
-        //if (length <=0){return "";}
 
         String alphabet = LOWERCASE;
-    
-                if (useUppercase) {
-                    alphabet += UPPERCASE;
-                }
-                
-                if (useDigits) {
-                    alphabet += DIGITS;
-                }
-                
-                if (useSpecial) {
-                    alphabet += SPECIAL;
-                }
+
+        if (useUppercase) {
+            alphabet += UPPERCASE;
+        }
+
+        if (useDigits) {
+            alphabet += DIGITS;
+        }
+
+        if (useSpecial) {
+            alphabet += SPECIAL;
+        }
 
         long current = seed;
 
         for (int i = 0; i < length; i++) {
-                
-                //int index = (int) (Math.random() * alphabet.length());                
+            int index = (int) (current % alphabet.length());
+            current = nextRandom(current);
 
-
-                int index = (int)(current % alphabet.length());
-                current = nextRandom(current);
-
-                result.append(alphabet.charAt(index));
-            }
-            return result.toString();
+            result.append(alphabet.charAt(index));
+        }
+        return result.toString();
     }
-    public static String checkPassword(String password){
-        int score = 0;
-    
-        boolean hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
 
-        if (password.length() >= 8) score++;
+    public static String checkPassword(String password) {
+        int score = 0;
+
+        boolean hasLower = false;
+        boolean hasUpper = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+
+        if (password.length() >= 8) {
+            score++;
+        }
 
         for (char c : password.toCharArray()) {
             if (!hasLower && LOWERCASE.indexOf(c) >= 0) {
@@ -62,20 +68,19 @@ public class PasswordGenerator {
                 hasSpecial = true;
                 score++;
             }
-            
+
             if (hasLower && hasUpper && hasDigit && hasSpecial) {
                 break;
             }
         }
 
-
         String result;
         switch (score) {
-            case 5 -> result = "Очень надёжный";
-            case 4 -> result = "Надёжный";
-            case 3 -> result = "Средний";
-            default -> result = "Слабый";
+            case 5: result = "Очень надёжный"; break;
+            case 4: result = "Надёжный"; break;
+            case 3: result = "Средний"; break;
+            default: result = "Слабый"; break;
         }
-        return result + " (оценка "+score+" из 5)";
+        return result + " (оценка " + score + " из 5)";
     }
 }
